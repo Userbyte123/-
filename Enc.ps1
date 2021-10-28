5ac0d63a1b8e4dcfbd2ef72c18d9e7414136c144
Userbyte123 <1776977899@qq.com>
2021-10-28 19:27:24 +0800

    sencond

---
+++ Enc.ps1
@@ -0,0 +1,17 @@
+function Encrypt($path) {
+  $script = Get-Content $path | Out-String
+  $secure = ConvertTo-SecureString $script -asPlainText -force
+  $export = $secure | ConvertFrom-SecureString
+  Set-Content $path $export
+}
+
+
+function Decrypt($path) {
+  $raw = Get-Content $path
+  $secure = ConvertTo-SecureString $raw
+  $helper = New-Object system.Management.Automation.PSCredential("test", $secure)
+  $plain = $helper.GetNetworkCredential().Password
+  Set-Content $path $plain
+}
+
+Encrypt D:\a.txt
